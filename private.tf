795e66e5c5455631b625b0326927716bc1d424b3
Igor Rodrigues <igorr2693@gmail.com>
2025-07-16 16:28:49 -0300

    fix: modifying project name in public.tf, private.tf and vpc.tf

---
+++ private.tf
@@ -0,0 +1,25 @@
+resource "aws_subnet" "eks-subnet-private-1a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
+  availability_zone = "${data.aws_region.current.region}a"
+  tags = merge(
+    local.tags,
+    {
+      Name                               = "${var.project_name}-priv-subnet-1a"
+      "kubernetes.io/role/internal-name" = 1
+    }
+  )
+}
+
+resource "aws_subnet" "eks-subnet-private-1b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
+  availability_zone = "${data.aws_region.current.region}b"
+  tags = merge(
+    local.tags,
+    {
+      Name                               = "${var.project_name}-priv-subnet-1b"
+      "kubernetes.io/role/internal-name" = 1
+    }
+  )
+}
