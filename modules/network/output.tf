963411268b3e387d5e1ef0a0662b9fffb3d5c5d2
Igor Rodrigues <igorr2693@gmail.com>
2025-07-17 10:43:52 -0300

    feat: output subnets

---
+++ modules/network/output.tf
@@ -0,0 +1,15 @@
+output "subnet-pub-1a" {
+  value = aws_subnet.eks-subnet-public-1a.id
+}
+
+output "subnet-pub-1b" {
+  value = aws_subnet.eks-subnet-public-1b.id
+}
+
+output "subnet-priv-1a" {
+  value = aws_subnet.eks-subnet-private-1a.id
+}
+output "subnet-priv-1b" {
+  value = aws_subnet.eks-subnet-private-1b.id
+}
+
